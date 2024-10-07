fb2bca17d972d301132a66b68580854699e3c312
Madusanka Madiligama <mbabeykoon@gmail.com>
2024-10-07 13:28:34 -0500

    add utils

--- src/so3d/utils/__init__.py
+++ src/so3d/utils/__init__.py
@@ -1,2 +1,4 @@
-from .array_signal_plotter import plot_signal
+from .array_signal_plotter import plot_receiver_signals, freq_plot, freq_plot_tcut, plot_seismic,plot_shot_source_signal
 from .segy_reader import read_segy
+from .data_utils import get_shot_data, get_line_data, get_receivers_data
+from .color_utils import redblue,redblue_cmap
